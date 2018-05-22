65dde28ce7c4550bce330a6b0954021a6dfccb29
leo <leo.ertuna@gmail.com>
2018-05-22 16:27:13 +0300

    Hooked adapter to a localhost solr cloud

--- moskito-core/src/main/java/net/anotheria/moskito/core/errorhandling/SolrClientAdapter.java
+++ moskito-core/src/main/java/net/anotheria/moskito/core/errorhandling/SolrClientAdapter.java
@@ -1,6 +1,7 @@
 package net.anotheria.moskito.core.errorhandling;
 
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
 
 /**
  * Helper adapter that will provide {@link SolrErrorCatcher} with the {@link SolrClient} object
@@ -9,7 +10,17 @@ import org.apache.solr.client.solrj.SolrClient;
  * @since 21.05.2018 17:52
  */
 public class SolrClientAdapter {
+    // Builder for solr client with default localhost url
+    private static HttpSolrClient.Builder builder = new HttpSolrClient.Builder("http://localhost:8983/solr");
+
+    // Solr client
+    private static SolrClient solrClient = builder.build();
+
+    /**
+     * Pointer to solr client
+     * @return solr client
+     */
     public static SolrClient getSolarClient() {
-        return null;
+        return solrClient;
     }
 }
