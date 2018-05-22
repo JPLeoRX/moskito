cc36da9a25a38a6b97e27c84cccc16996257ef1e
leo <leo.ertuna@gmail.com>
2018-05-22 16:06:50 +0300

    Fields are assigned to special setters; tags are stored as json strings

--- moskito-core/src/main/java/net/anotheria/moskito/core/errorhandling/SolrCaughtError.java
+++ moskito-core/src/main/java/net/anotheria/moskito/core/errorhandling/SolrCaughtError.java
@@ -3,29 +3,28 @@ package net.anotheria.moskito.core.errorhandling;
 import org.apache.solr.client.solrj.beans.Field;
 
 import java.io.Serializable;
-import java.util.Map;
+import java.util.ArrayList;
 
 /**
- * This class is a representation of {@link CaughtError} object specifically tailored to be used with Solr
+ * This class is a representation of {@link CaughtError} object specifically tailored to be used with Solr.
+ *
+ * Solr always returns a multipart field in the form of an array list, so it requires an additional setter that will extract the first element of this list.
+ *
+ * We keep timestamp, but we add throwable class name and message as well as JSON serialized version of throwable.
+ * Since Solr has problems with storing tags - map of tags is also serialized to JSON.
+ *
+ * We can create this object from {@link CaughtError} and convert back to caught error from this object.
+ * For more details about conversion take a look at {@link SolrCaughtErrorConverter}
  *
  * @author Leo Ertuna
  * @since 21.03.2018 17:30
  */
 public class SolrCaughtError implements Serializable {
-    @Field
     private long timestamp;
-
-    @Field
     private String throwableClassName;
-
-    @Field
     private String throwableMessage;
-
-    @Field
     private String throwableJson;
-
-    @Field("key*")
-    private Map<String, String> tags;
+    private String tagsJson;
 
     /**
      * Default constructor
@@ -51,6 +50,15 @@ public class SolrCaughtError implements Serializable {
     }
 
     /**
+     * Setter used by solr
+     * @param timestamp solr list
+     */
+    @Field
+    public void setTimestamp(ArrayList<Long> timestamp) {
+        this.timestamp = timestamp.get(0);
+    }
+
+    /**
      * Getter
      * @return the name of the throwable class
      */
@@ -64,6 +72,15 @@ public class SolrCaughtError implements Serializable {
      */
     public void setThrowableClassName(String throwableClassName) {
         this.throwableClassName = throwableClassName;
+    }
+
+    /**
+     * Setter used by solr
+     * @param throwableClassName solr list
+     */
+    @Field
+    public void setThrowableClassName(ArrayList<String> throwableClassName) {
+        this.throwableClassName = throwableClassName.get(0);
     }
 
     /**
@@ -83,6 +100,16 @@ public class SolrCaughtError implements Serializable {
     }
 
     /**
+     * Setter used by solr
+     * @param throwableMessage solr list
+     */
+    @Field
+    public void setThrowableMessage(ArrayList<String> throwableMessage) {
+        this.throwableMessage = throwableMessage.get(0);
+    }
+
+
+    /**
      * Getter
      * @return json string of the throwable
      */
@@ -99,19 +126,37 @@ public class SolrCaughtError implements Serializable {
     }
 
     /**
+     * Setter used by solr
+     * @param throwableJson solr list
+     */
+    @Field
+    public void setThrowableJson(ArrayList<String> throwableJson) {
+        this.throwableJson = throwableJson.get(0);
+    }
+
+    /**
      * Getter
      * @return tags
      */
-    public Map<String, String> getTags() {
-        return tags;
+    public String getTagsJson() {
+        return tagsJson;
     }
 
     /**
      * Setter
-     * @param tags new tags
+     * @param tagsJson new tags
      */
-    public void setTags(Map<String, String> tags) {
-        this.tags = tags;
+    public void setTagsJson(String tagsJson) {
+        this.tagsJson = tagsJson;
+    }
+
+    /**
+     * Setter used by solr
+     * @param tags solr list
+     */
+    @Field
+    public void setTagsAsJson(ArrayList<String> tags) {
+        this.tagsJson = tags.get(0);
     }
 
     /**
@@ -125,7 +170,7 @@ public class SolrCaughtError implements Serializable {
                 ", throwableClassName='" + throwableClassName + '\'' +
                 ", throwableMessage='" + throwableMessage + '\'' +
                 ", throwableJson='" + throwableJson + '\'' +
-                ", tags=" + tags +
+                ", tagsJson=" + tagsJson +
                 '}';
     }
 }
